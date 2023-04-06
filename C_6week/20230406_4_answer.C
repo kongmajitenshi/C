28af2cc6dce2a32817352cd1c804222951146c89
kongmajitenshi <128122803+kongmajitenshi@users.noreply.github.com>
2023-04-06 21:41:08 +0900

    Create 20230406_4_answer.C

---
+++ C_6week/20230406_4_answer.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i=1, sum=0, n, a, b;
+    scanf("%d", &n);
+    
+    while(i<=n)
+    {
+        scanf("%d %d", &a, &b);
+        printf("%d + %d = %d", a, b, a+b);
+        i++;
+        printf("\n");
+    }
+    return 0;
+}
