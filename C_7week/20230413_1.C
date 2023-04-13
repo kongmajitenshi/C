223a85a941a864ced43832468a077d6490d79221
kongmajitenshi <128122803+kongmajitenshi@users.noreply.github.com>
2023-04-13 22:14:04 +0900

    Create 20230413_1.C

---
+++ C_7week/20230413_1.C
@@ -0,0 +1,20 @@
+```
+#include <stdio.h>
+
+void print_stars();
+{
+  for(i=0; i<30; i++)
+  {
+    printf("*");
+  }
+}
+
+int main()
+{
+  print_stars();
+  printf("\nHelloWrold!\n");
+  print_stars();
+  printf("\n");
+  return 0;
+}
+```
