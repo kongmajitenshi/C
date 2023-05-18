2c1936b622a74ce5267c8ea93da4ef8fc4ee4418
kongmajitenshi <128122803+kongmajitenshi@users.noreply.github.com>
2023-05-18 13:44:41 +0900

    Update drawing_trigonometric_function.C

--- C_11week/assignment/drawing_trigonometric_function.C
+++ C_11week/assignment/drawing_trigonometric_function.C
@@ -18,11 +18,12 @@ int main(void)
 {
     int degree, x, y;
 
-    for (degree = 0; degree <= 90; degree += 10) // 싸인값은 -1.0에서 1.0이므로 정수로 반올림하여서 증폭한다.
+    for (degree = 0; degree <= 90; degree += 10) // 싸인값은 -1.0에서 1.0이므로 정수로 반올림하여서 증폭한다. 
+        //degree 값을 180도까지 해주어야 정상적으로 출력됨. sin함수 한 사이클이 360도이므로 90도까지 설정 시 1/4만 출력됨
     { 
         y = (double)(60 * sin(rad((double)degree)) + 0.5); // 교안 출력 결과와 실제 출력 결과가 달라서 double형으로 변경해보았으나 차이가 없었음.
         drawbar(y); 
     }
 
     return 0;
-}
+} // degree값이 잘못되어서 출력이 이상하게 되었음
